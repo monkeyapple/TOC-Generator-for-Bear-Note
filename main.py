d1d4100dd24f67a98557cb042d826752b7ea7fa8
AppleMonkey <monkeyappleh@gmail.com>
2020-07-07 22:52:14 +0800

    extract Headers and arrange

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
-from urllib.request import urlopen
+
 from bs4 import BeautifulSoup
-html=urlopen('')
-
-
-# from bs4 import BeautifulSoup
-# sum=0
-#
-# soup = BeautifulSoup(open('D:\Coding\GitHub\PycharmProjects\Bear-TOC\testFile.html',encoding = 'utf-8', errors='ignore'),'lxml')
-# items=soup.findAll('h2')
-#
-#
-# # for i in items:
-# #     result=i.text.split("¥")[1]
-# #     result=int(result)
-# #     sum+=result
-#
-# csvFile = open('wjdata.csv', 'w',newline='')
-# try:
-#     writer = csv.writer(csvFile)
-#     writer.writerow(('ID','Amount'))
-#     for idx,i in enumerate(items):
-#         result = int(i.text.split("¥")[1])
-#         writer.writerow((idx+1, result))
-#
-#
-# finally:
-#     csvFile.close()
+url=open("testFile.html",encoding = 'utf-8', errors='ignore')
+bs=BeautifulSoup(url,'lxml')
+collectList=bs.find_all(['h1','h2','h3','h4'])
+for i in collectList:
+    if i.nmae=='h1':
+        print(i.get_text())
+    elif i.name=='h2':
+        print(" "+i.get_text())
+    elif i.name=='h3':
+        print("  "+i.get_text())
+    elif i.name=='h4':
+        print("   "+i.get_text())
