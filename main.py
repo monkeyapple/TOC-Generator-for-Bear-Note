b3e657e5c3eedf6669122d6b2ec14c91953ea5ce
AppleMonkey <monkeyappleh@gmail.com>
2020-07-27 23:07:25 +0800

    fixed bug
    1.html.parser
    2.bs4 url fixed

--- main.py
+++ main.py
@@ -8,47 +8,51 @@ class BearTOC(QMainWindow):
         super().__init__()
         self.ui=Ui_MainWindow()
         self.ui.setupUi(self)
-        self.initializing()
+        # self.initializing()
         self.ui.htmlDir.clicked.connect(self.select_html)
         self.ui.runButton.clicked.connect(self.scraping)
         self.ui.copyAll.clicked.connect(self.selectAndCopy)
+        self.htmlFilePath=('','')
+        self.scrapResult=''
+
 
 # ----------------Functions------------------------
-    def initializing(self):
-        self.htmlFilePath=None
-        self.scrapResult=""
 
     def select_html(self):
         self.htmlFilePath=QFileDialog.getOpenFileName(self,'Select HTML File','/home','HTML file(*.html)')
+
         if self.htmlFilePath[0]:
             self.ui.label_htmlDir.setText(self.htmlFilePath[0])
 
     def scraping(self):
-        if self.htmlFilePath[0]:
-            self.htmlData=open(self.htmlFilePath[0])
-            url = open("testFile.html", encoding='utf-8', errors='ignore')
-            bs = BeautifulSoup(url, 'lxml')
+        self.ui.textBrowser.clear()
+        self.scrapResult = ''
+        if self.htmlFilePath[0]!='' and len(self.ui.identifier.text())!=0:
+            url = open(self.htmlFilePath[0], encoding='utf-8', errors='ignore')
+            bs = BeautifulSoup(url, 'html.parser')
             self.collectList = bs.find_all(['h1', 'h2', 'h3', 'h4'])
 
-        self.scrapResult="# Table of Contents\n"
-        for i in self.collectList:
-            if i.name=='h1':
-                insert=i.get_text()
-                self.scrapResult+='*'+' '+'['+insert+']'+'('+self.bearNoteUrl(insert)+')\n'
-            elif i.name=='h2':
-                insert = i.get_text()
-                self.scrapResult+='   '+'['+insert+']'+'('+self.bearNoteUrl(insert)+')\n'
-            elif i.name=='h3':
-                insert = i.get_text()
-                self.scrapResult+='    '+'['+insert+']'+'('+self.bearNoteUrl(insert)+')\n'
-            elif i.name=='h4':
-                insert = i.get_text()
-                self.scrapResult+='     '+'['+insert+']'+'('+self.bearNoteUrl(insert)+')\n'
-        self.scrapResult +='***\n'
-        self.ui.textBrowser.append(self.scrapResult)
+            self.scrapResult="# Table of Contents\n"
+            for i in self.collectList:
+                if i.name=='h1':
+                    insert=i.get_text()
+                    self.scrapResult+='*'+' '+'['+insert+']'+'('+self.bearNoteUrl(insert)+')\n'
+                elif i.name=='h2':
+                    insert = i.get_text()
+                    self.scrapResult+='   '+'['+insert+']'+'('+self.bearNoteUrl(insert)+')\n'
+                elif i.name=='h3':
+                    insert = i.get_text()
+                    self.scrapResult+='    '+'['+insert+']'+'('+self.bearNoteUrl(insert)+')\n'
+                elif i.name=='h4':
+                    insert = i.get_text()
+                    self.scrapResult+='     '+'['+insert+']'+'('+self.bearNoteUrl(insert)+')\n'
+            self.scrapResult +='***\n'
+            self.ui.textBrowser.append(self.scrapResult)
+        else:
+            QMessageBox.warning(self, 'Warning', 'identifier or html file needed!')
 
     def bearNoteUrl(self,title):
-        if self.ui.identifier.text()!=0:
+        if len(self.ui.identifier.text())!=0:
             id=self.ui.identifier.text()
             self.noteUrl='bear://x-callback-url/open-note?id='+id+'&header='+title
             return(self.noteUrl)
@@ -56,15 +60,18 @@ class BearTOC(QMainWindow):
             QMessageBox.warning(self, 'Warning', 'Please provide the identifier!')
 
     def selectAndCopy(self):
-        if self.ui.textBrowser:
+        if len(self.scrapResult)!=0:
             QApplication.clipboard().setText(self.scrapResult)
             self.finishedPopup()
+        else:
+            pass
 
     def finishedPopup(self):
         msg=QMessageBox()
         msg.setWindowTitle("Info")
         msg.setText("Copied!")
         msg.exec_()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
